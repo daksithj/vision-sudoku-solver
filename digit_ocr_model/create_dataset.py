8edf4efaa37f14e1f79390bd12a403d711ce13cd
daksithj <daksithj@gmail.com>
2021-04-18 20:33:53 +0530

    Cleaned the ocr dataset generator

--- digit_ocr_model/create_dataset.py
+++ digit_ocr_model/create_dataset.py
@@ -1,73 +1,12 @@
-import os
 import cv2
 import numpy as np
 import random
-import pickle as pkl
 import imutils
 from keras.utils import Sequence, np_utils
 
-size = 28
-data_count = 100000
-output_dir = "ocr_dataset/"
 
 fonts = [cv2.FONT_HERSHEY_SIMPLEX, cv2.FONT_HERSHEY_COMPLEX, cv2.FONT_HERSHEY_DUPLEX, cv2.FONT_HERSHEY_SCRIPT_COMPLEX,
          cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, cv2.FONT_HERSHEY_TRIPLEX, cv2.FONT_ITALIC]
-
-
-def start_generating():
-
-    count = 0
-
-    numbers = []
-
-    data_per_num = int(data_count / 10)
-
-    number_count = [data_per_num for _ in range(10)]
-
-    possible_numbers = [i for i in range(10)]
-
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
-
-    while count < data_count:
-
-        num = random.choice(possible_numbers)
-
-        if number_count[num] == 0:
-            possible_numbers.remove(num)
-            continue
-
-        generate_number(count, num, numbers)
-        count += 1
-
-    with open('number_list.pkl', 'wb') as f:
-        pkl.dump(numbers, f)
-
-
-def generate_number(count, num, numbers):
-    x_loc = random.uniform(0.1, 0.5)
-
-    scale = size / random.uniform(27, 50)
-
-    thickness = random.randint(1, 2)
-
-    text_x = int(size * x_loc)
-    text_y = int(size * -0.3) + size
-
-    canvas = np.zeros((size, size, 3))
-
-    canvas = cv2.putText(canvas, str(num), (text_x, text_y), random.choice(fonts), scale, (1, 1, 1), thickness)
-
-    canvas = canvas * 255
-    canvas = np.asarray(canvas).astype(np.uint8)
-
-    rotate_chance = random.randint(0, 100)
-
-    if rotate_chance > 75:
-        canvas = imutils.rotate(canvas, random.randint(-30, 30))
-
-    numbers.append((count, num))
-    cv2.imwrite(f'{output_dir}{count}.png', canvas)
 
 
 class NumberDataSet(Sequence):
@@ -156,11 +95,3 @@ class NumberDataSet(Sequence):
 
         canvas = np.expand_dims(canvas, axis=2)
         return canvas
-
-
-
-if __name__ == '__main__':
-    data_set = NumberDataSet(10000, 200, 64)
-
-    data, num = (data_set.__getitem__(0))
-    print(data.shape)
--- digit_ocr_model/model.py
+++ digit_ocr_model/model.py
@@ -1,21 +1,17 @@
-import numpy as np
-import pickle as pkl
-import cv2
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint
-from tensorflow.python.keras import Sequential
+from keras import Sequential
 from digit_ocr_model.create_dataset import NumberDataSet
 
 
 data_size = 70000
 batch_size = 200
 image_dim = 64
+epochs = 250
 
 num_data_Set = NumberDataSet((data_size // batch_size), batch_size, image_dim)
-# Create model
-# Building CNN
+
 model = Sequential()
 model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(image_dim, image_dim, 1)))
 model.add(MaxPooling2D((2, 2)))
@@ -31,9 +27,12 @@ model.add(Dense(10, activation='softmax'))
 save_call = ModelCheckpoint("digit_model.h5", monitor='val_loss', verbose=0, save_best_only=False,
                             save_weights_only=False, mode='auto', period=1)
 
-# compile model
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
 print('Started training')
-model.fit(num_data_Set, callbacks=[save_call], epochs=500)
+
+model.fit(num_data_Set, callbacks=[save_call], epochs=epochs)
+
+print("Finished training")
 
 model.save("digit_model.h5")
